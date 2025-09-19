af53a5b88186e186c4c6d154979448ef850adf92
DESKTOP-MM0J0LH\tanvietvo <viet.vo201@hcmut.edu.vn>
2025-09-19 15:15:40 +0700

    format code

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -87,7 +87,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-  int counter = 0;
+  int counter = 1;
   /* USER CODE END 2 */
 
   /* Infinite loop */
